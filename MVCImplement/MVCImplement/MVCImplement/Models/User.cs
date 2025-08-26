3436f5722737342a66ab8a268cef8ce496581b79
GiaHuy2505 <phangiahuy25052003@gmail.com>
2025-08-26 10:10:26 +0700

    Update UserModel and UserDto

---
+++ MVCImplement/MVCImplement/MVCImplement/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCImplement.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
